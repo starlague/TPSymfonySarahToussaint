3d7aa97816a08ced42f422a1c20d0232fb1cf961
starlague <nathalie.tuernal@gmail.com>
2025-12-14 15:01:15 +0100

    Ajout de l'entité user, de l'inscription et de la connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,23 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('home/index.html.twig', [
             'web_site_name' => 'carnet d\'amis',
         ]);
+    }
+
+    #[Route('/amis', name: 'app_liste_amis')]
+    public function listeAmis(): Response {
+        return $this->render('home/listeAmis.html.twig');
+    }
+
+    #[Route('/inscription', name:'app_inscription')]
+    public function inscription(): Response {
+        return $this->render('home/inscription.html.twig');
+    }
+    #[Route('/connexion', name:'app_connexion')]
+    public function connexion(): Response {
+        return $this->render('home/connexion.html.twig');
     }
 }
